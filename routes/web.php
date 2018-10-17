62da1c4264705d64d8a0cac9d67f522482a8f29b
hualu <834739358@qq.com>
2018-10-17 23:02:58 +0800

    code optimize

--- routes/web.php
+++ routes/web.php
@@ -42,3 +42,7 @@ Route::get('redirect', function() {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
